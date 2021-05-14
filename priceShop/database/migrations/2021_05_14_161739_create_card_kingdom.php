1b681420dad9b8e1cdc122a5ef1afdb696a2d5ec
carovi0696 <yabsurdo2op>
2021-05-14 19:40:58 +0200

    made migrations

---
+++ priceShop/database/migrations/2021_05_14_161739_create_card_kingdom.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardKingdom extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card_kingdom', function (Blueprint $table) {
+            $table->id('id')->autoIncrement();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card_kingdom');
+    }
+}
---
+++ priceShop/database/migrations/2021_05_14_171721_create_magic_card.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMagicCard extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('magic_card', function (Blueprint $table) {
+            $table->id('id')->autoIncrement();
+            $table->string('name');
+            $table->unsignedBigInteger('cardkingdom_id');
+            $table->foreign('cardkingdom_id')->references('id')->on('card_kingdom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('magic_card');
+    }
+}
